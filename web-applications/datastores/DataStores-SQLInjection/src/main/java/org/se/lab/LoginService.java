70e07fbac4c064f9ccacc23bcb76d348d17526c9
teiniker <egon.teiniker@gmail.com>
2019-05-03 17:24:52 +0200

    Refactoring.

--- web-applications/datastores/DataStores-SQLInjection/src/main/java/org/se/lab/LoginService.java
+++ web-applications/datastores/DataStores-SQLInjection/src/main/java/org/se/lab/LoginService.java
@@ -23,37 +23,20 @@ public class LoginService
      * The java.sql.PreparedStatement class properly escapes input strings, 
      * preventing SQL injection when used properly.
      */
-    public boolean login(Connection c, String username, String password) 
+    public boolean login(Connection connection, String username, String password)
     {
         final String SQL = "SELECT id FROM User WHERE username=? AND password=?";
         
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try
+        try(PreparedStatement pstmt = connection.prepareStatement(SQL))
         {
-            pstmt = c.prepareStatement(SQL);
             pstmt.setString(1, username);
             pstmt.setString(2, password);
-            rs = pstmt.executeQuery();
+            ResultSet rs = pstmt.executeQuery();
             return rs.next();
         }
         catch(SQLException e)
         {
         	return false;
-        }
-        finally
-        {
-        	try
-        	{
-	            if (rs != null)
-	                rs.close();
-	            if (pstmt != null)
-	                pstmt.close();
-        	}
-        	catch(SQLException e)
-        	{
-        		return false;
-        	}
         }
     }
 }
