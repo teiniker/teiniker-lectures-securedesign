ce6bb95b788241aeac23368b59fae73d350fa267
teiniker <egon.teiniker@gmail.com>
2019-05-03 17:23:06 +0200

    Refactoring.

--- web-applications/datastores/PathTraversal-Attack-Exercise/src/main/java/org/se/lab/FileManager.java
+++ web-applications/datastores/PathTraversal-Attack-Exercise/src/main/java/org/se/lab/FileManager.java
@@ -1,32 +1,31 @@
 package org.se.lab;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class FileManager
 {
 	private static final String BASE_DIRECTORY = "./doc";
 	
 	public String readFile(final String name) 
-		throws IOException
 	{
 		if(name == null)
 			throw new IllegalArgumentException();
-			
+
 		File file = new File(BASE_DIRECTORY, name);
-		FileReader fileReader = new FileReader(file);
-		BufferedReader in = new BufferedReader(fileReader);
 
-		StringBuilder buffer = new StringBuilder();
-		String line;
-		while ((line = in.readLine()) != null)
+		try(BufferedReader in = new BufferedReader(new FileReader(file)))
 		{
-			buffer.append(line).append("\n");
+			StringBuilder buffer = new StringBuilder();
+			String line;
+			while ((line = in.readLine()) != null)
+			{
+				buffer.append(line).append("\n");
+			}
+			return buffer.toString();
 		}
-		in.close();
-
-		return buffer.toString();
+		catch (IOException e)
+		{
+			throw new IllegalStateException("Can't access file " + name, e);
+		}
 	}
 }
