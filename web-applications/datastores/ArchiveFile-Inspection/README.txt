ff1d804e5304d5bc44c61849b1edb120318a8eca
teiniker <egon.teiniker@gmail.com>
2020-05-21 23:07:28 +0200

    Initial commit.

---
+++ web-applications/datastores/ArchiveFile-Inspection/README.txt
@@ -0,0 +1,7 @@
+ZIP Bomb
+---------------------------------------------------------------------------------
+
+A zip bomb is a malicious zip file that is of small size compressed but it is of
+a giant size uncompressed.
+
+See: 42.zip https://unforgettable.dk/
---
+++ web-applications/datastores/ArchiveFile-Inspection/src/test/java/org/se/lab/ZipInspectionTest.java
@@ -0,0 +1,35 @@
+package org.se.lab;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ZipInspectionTest
+{
+	private static final String BASE_DIRECTORY = "./src/test/resources";
+
+	@Test
+	public void testZipFile() throws IOException
+	{
+		File file = new File(BASE_DIRECTORY,"demo.zip");
+		long compressedSize = file.length();
+		System.out.println("Compressed size         = " + compressedSize);
+
+		ZipFile zip = new ZipFile(file);
+
+		long totalUncompressedSize = 0;
+		Enumeration<? extends ZipEntry> entry = zip.entries();
+		while(entry.hasMoreElements())
+		{
+			ZipEntry e = entry.nextElement();
+			totalUncompressedSize += e.getSize();
+		}
+		System.out.println("Total Uncompressed Size = " + totalUncompressedSize);
+	}
+}
