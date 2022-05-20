389fa084496f626a9c72f17360bd65ab99ac4c9b
teiniker <egon.teiniker@gmail.com>
2022-05-20 17:17:20 +0200

    Refactoring.

--- secure-design/secure-communication/http/springboot-mvc-translator/src/main/java/org/se/lab/TranslatorController.java
+++ secure-design/secure-communication/http/springboot-mvc-translator/src/main/java/org/se/lab/TranslatorController.java
@@ -1,6 +1,5 @@
 package org.se.lab;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
