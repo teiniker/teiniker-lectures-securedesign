8f2c5fcb01d419139f523ad216551c5e89e88ae9
teiniker <egon.teiniker@fh-joanneum.at>
2024-02-11 18:38:47 +0100

    Moved examples to software architecture repo.

--- secure-design/web-services/api-security-basics/api-bestpractices/SpringBoot-HATEOAS/src/main/java/org/se/lab/data/ArticleNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.se.lab.data;
-
-public class ArticleNotFoundException extends RuntimeException
-{
-    public ArticleNotFoundException(Long id)
-    {
-        super("Could not find article " + id);
-    }
-}
