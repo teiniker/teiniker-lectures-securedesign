1b6dde35865596c9a13a9b189cde71103d06cfc6
teiniker <egon.teiniker@gmail.com>
2025-02-10 17:44:50 +0100

    Refactoring.

--- secure-design/web-services/api-security-basics/api-authorization/SpringBoot-Book-Authorization-Exercise/src/main/java/org/se/lab/Application.java
+++ secure-design/web-services/api-security-basics/api-authorization/SpringBoot-Book-Authorization-Exercise/src/main/java/org/se/lab/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootBookServiceApplication
+public class Application
 {
 	public static void main(String[] args)
 	{
-		SpringApplication.run(SpringBootBookServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
