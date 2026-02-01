58817ddaab2a6a26293b49c29ea40ee4995a915b
teiniker <egon.teiniker@gmail.com>
2026-02-01 08:34:56 +0100

    Refactoring: Added comments.

--- secure-design/web-services/introduction/SpringBoot-REST-JPA/src/main/java/org/se/lab/BookRepository.java
+++ secure-design/web-services/introduction/SpringBoot-REST-JPA/src/main/java/org/se/lab/BookRepository.java
@@ -2,6 +2,7 @@ package org.se.lab;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Data Access Object (DAO)
 public interface BookRepository extends JpaRepository<Book, Long>
 {
     // Additional query methods (if needed) can be defined here.
